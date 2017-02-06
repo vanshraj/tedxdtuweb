9339b4c6bb38ec77120f7dc1c3f9c5d52ecef126
vanshraj <vanshajbehl96@gmail.com>
2017-02-06 10:34:42 +0530

    reset password added

--- profile/js/profile.js
+++ profile/js/profile.js
@@ -1,3 +1,8 @@
 $('#view_profile').click(function(){
 	$('.profile').slideToggle("slow", "linear");
+	$('.reset').slideUp("slow", "linear");
+});
+$('#view_reset').click(function(){
+	$('.reset').slideToggle("slow", "linear");
+	$('.profile').slideUp("slow", "linear");
 });
